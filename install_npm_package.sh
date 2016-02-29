705ffa1e5d9acc521e9a35bf8964bfa1679e95d2
EthanCai <ethancai@qq.com>
2016-02-29 17:07:58 +0800

    add npm install script

---
+++ install_npm_package.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+npm install -g npm
+npm install -g cnpm --registry=https://registry.npm.taobao.org
+cnpm install -g bower
+cnpm install -g webpack
+cnpm install -g http-server
+cnpm install -g node-uuid
