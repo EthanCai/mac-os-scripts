3862ec07b4604cba537e9573cfe49d3aca16b1cf
caichengyang <caichengyang@augmn.com>
2016-08-17 18:04:06 +0800

    adjust install-go-pkg.sh

--- tools/install-go-pkg.sh
+++ tools/install-go-pkg.sh
@@ -8,7 +8,30 @@ IFS=$'\n\t'
 CUR_DIR=$(cd $(dirname $0) && pwd)
 source $CUR_DIR/../lib/common.sh
 
+# this funciton is used to install golang.org/x/pkg
+function installGoXPkg() {
+    typeset pkgName
+    typeset pkgURL
+    pkgName=$1
+    pkgURL=$2
+
+    pkgPath="$GOPATH/src/$pkgName"
+    createDir "$(dirname "$pkgPath")" # create parent directory of pkgPath
+
+    if [ -d "$pkgPath" ]
+    then
+        info "update $pkgName"
+        pushd "$pkgPath" 1 > /dev/null
+        git pull
+        popd 1 > /dev/null
+    else
+        info "download $pkgName"
+        git clone "$pkgURL" "$pkgPath" 2> /dev/null || info "$pkgName is already installed"
+    fi
+}
+
 # install golang.org/x package
-createDir "$GOPATH/src/golang.org/x"
-git clone https://github.com/golang/sys.git "$GOPATH/src/golang.org/x/sys" 2> /dev/null || info "golang.org/x/sys is already installed" # install golang.org/x/sys
-git clone https://github.com/golang/net.git "$GOPATH/src/golang.org/x/net" 2> /dev/null || info "golang.org/x/net is already installed" # install golang.org/x/net
+installGoXPkg "golang.org/x/sys" "https://github.com/golang/sys.git"
+installGoXPkg "golang.org/x/net" "https://github.com/golang/net.git"
+installGoXPkg "golang.org/x/text" "https://github.com/golang/text.git"
+installGoXPkg "golang.org/x/crypto" "https://github.com/golang/crypto.git"
