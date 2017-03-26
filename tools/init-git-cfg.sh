6af569e74254fc637725770a859e39d25d156ac8
ethancai <ethancai@qq.com>
2017-03-26 16:32:01 +0800

    save

---
+++ tools/init-git-cfg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Use unofficial Bash Strict Mode
+set -euo pipefail
+IFS=$'\n\t'
+
+# visualizing branch topology in git
+# refer to: http://stackoverflow.com/questions/1838873/visualizing-branch-topology-in-git/34467298
+git config --global alias.lg1 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'"
+git config --global alias.lg2 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'"
+git config --global alias.lg3 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'"
